5f56f90156cdc04c6fec53f46d4dc0cf11207474
Mr. AQ <a.q619619@gmail.com>
2022-07-25 20:32:07 +0500

    First commit

---
+++ Weather-App/src/app/weather-widget/weather-widget.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-widget',
+  templateUrl: './weather-widget.component.html',
+  styleUrls: ['./weather-widget.component.css']
+})
+export class WeatherWidgetComponent implements OnInit {
+
+  WeatherData:any;
+  constructor() { }
+
+  ngOnInit() {
+    this.WeatherData = {
+      main : {},
+      isDay: true
+    };
+    console.log(this.WeatherData);
+  }
+  getWeatherNewData(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=lahore&appid=abc0a072fb26e421d7f8926cd1e00912')
+    .then(response=>response.json())
+    .then(data=>{console.log('weather', data);})
+
+    // let data = JSON.parse('{"coord":{"lon":72.85,"lat":19.01},"weather":[{"id":721,"main":"Haze","description":"haze","icon":"50n"}],"base":"stations","main":{"temp":297.15,"feels_like":297.4,"temp_min":297.15,"temp_max":297.15,"pressure":1013,"humidity":69},"visibility":3500,"wind":{"speed":3.6,"deg":300},"clouds":{"all":20},"dt":1580141589,"sys":{"type":1,"id":9052,"country":"IN","sunrise":1580089441,"sunset":1580129884},"timezone":19800,"id":1275339,"name":"Mumbai","cod":200}');
+    // this.setWeatherData(data);
+  }
+  getWeatherData(location :any){
+    fetch(`http://api.weatherapi.com/v1/current.json?key=d65762d2a6124cb88e8112942222507&q=${location.City}&aqi=no`)
+    .then(response=>response.json())
+    .then(data=>{
+      console.log(data);
+      this.setWeatherData(data);})
+
+    // let data = JSON.parse('{"coord":{"lon":72.85,"lat":19.01},"weather":[{"id":721,"main":"Haze","description":"haze","icon":"50n"}],"base":"stations","main":{"temp":297.15,"feels_like":297.4,"temp_min":297.15,"temp_max":297.15,"pressure":1013,"humidity":69},"visibility":3500,"wind":{"speed":3.6,"deg":300},"clouds":{"all":20},"dt":1580141589,"sys":{"type":1,"id":9052,"country":"IN","sunrise":1580089441,"sunset":1580129884},"timezone":19800,"id":1275339,"name":"Mumbai","cod":200}');
+    // this.setWeatherData(data);
+  }
+
+  setWeatherData(data: any){
+    this.WeatherData = data;
+    let sunsetTime = new Date(this.WeatherData.sys.sunset * 1000);
+    this.WeatherData.sunset_time = sunsetTime.toLocaleTimeString();
+    let currentDate = new Date();
+    this.WeatherData.isDay = (currentDate.getTime() < sunsetTime.getTime());
+    this.WeatherData.temp_celcius = (this.WeatherData.main.temp - 273.15).toFixed(0);
+    this.WeatherData.temp_min = (this.WeatherData.main.temp_min - 273.15).toFixed(0);
+    this.WeatherData.temp_max = (this.WeatherData.main.temp_max - 273.15).toFixed(0);
+    this.WeatherData.temp_feels_like = (this.WeatherData.main.feels_like - 273.15).toFixed(0);
+  }
+
+
+}
